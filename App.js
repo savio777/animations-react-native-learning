8f573c9591fd9f6c2fbd769a8806074d9b409706
savio <saviopf22@gmail.com>
2020-08-15 01:15:44 -0300

    testando eixo x

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
 import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Animated} from 'react-native';
 
+const TRANSFORM_WHITELIST = {
+  translateX: true,
+  translateY: true,
+  scale: true,
+  scaleX: true,
+  scaleY: true,
+  rotate: true,
+  rotateX: true,
+  rotateY: true,
+  rotateZ: true,
+  perspective: true,
+};
+
 const App = () => {
   const [ballY, setBallY] = useState(new Animated.Value(0));
   const [ballX, setBallX] = useState(new Animated.Value(0));
 
   useEffect(() => {
-    Animated.timing(ballY, {toValue: 500, duration: 1000}).start();
+    Animated.timing(ballY, {
+      toValue: 500,
+      duration: 1000,
+      //useNativeDriver: true,
+    }).start();
+    setTimeout(() => {
+      Animated.timing(ballX, {
+        toValue: 100,
+        duration: 1000,
+      }).start();
+    }, 1000);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.ball, {top: ballY}]}></Animated.View>
+      <Animated.View
+        style={[styles.ball, {top: ballY, left: ballX}]}></Animated.View>
     </View>
   );
 };
